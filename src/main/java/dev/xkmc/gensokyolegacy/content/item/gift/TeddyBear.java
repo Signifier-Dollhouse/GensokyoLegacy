486579633eca289d1f7c6b8a3b4cabf4e111233d
Crystal1921 <90130465+Crystal1921@users.noreply.github.com>
2026-08-18 19:56:53 +0800

    parchment

---
+++ src/main/java/dev/xkmc/gensokyolegacy/content/item/gift/TeddyBear.java
@@ -0,0 +1,9 @@
+package dev.xkmc.gensokyolegacy.content.item.gift;
+
+import net.minecraft.world.item.Item;
+
+public class TeddyBear extends Item {
+    public TeddyBear(Properties properties) {
+        super(properties);
+    }
+}
