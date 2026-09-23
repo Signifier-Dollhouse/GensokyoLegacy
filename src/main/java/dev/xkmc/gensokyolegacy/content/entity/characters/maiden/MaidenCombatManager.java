f17a8cd63b58b83beb73eef3b08ef59f62c9dfa2
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-09-23 10:31:23 +0800

    reimu behavior

--- src/main/java/dev/xkmc/gensokyolegacy/content/entity/characters/maiden/MaidenCombatManager.java
+++ src/main/java/dev/xkmc/gensokyolegacy/content/entity/characters/maiden/MaidenCombatManager.java
@@ -6,9 +6,13 @@ import dev.xkmc.gensokyolegacy.content.entity.youkai.GeneralYoukaiEntity;
 import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.monster.Enemy;
 import net.minecraft.world.entity.npc.Villager;
 
 class MaidenCombatManager extends DefaultCombatManager {
+
+	// proactive extermination (FIGHT) while keeping prey heal effects, unlike ENEMY
+	private static final TargetKind EXTERMINATE = new TargetKind(true, true, false);
 
 	public MaidenCombatManager(MaidenEntity e) {
 		super(e, GeneralYoukaiEntity.SPELL);
@@ -17,9 +21,9 @@ class MaidenCombatManager extends DefaultCombatManager {
 	@Override
 	public TargetKind targetKind(LivingEntity le) {
 		if (le.getType().is(EntityTypeTags.RAIDERS))
-			return TargetKind.PRAY;
-		if (le instanceof Mob mob && (mob.getTarget() instanceof Villager || mob.getLastHurtMob() instanceof Villager))
-			return TargetKind.PRAY;
+			return EXTERMINATE;
+		if (le instanceof Mob mob && mob instanceof Enemy && (mob.getTarget() instanceof Villager || mob.getLastHurtMob() instanceof Villager))
+			return EXTERMINATE;
 		return super.targetKind(le);
 	}
 
--- src/main/java/dev/xkmc/gensokyolegacy/content/entity/characters/maiden/MaidenEntity.java
+++ src/main/java/dev/xkmc/gensokyolegacy/content/entity/characters/maiden/MaidenEntity.java
@@ -1,7 +1,10 @@
 package dev.xkmc.gensokyolegacy.content.entity.characters.maiden;
 
+import dev.xkmc.gensokyolegacy.content.entity.behavior.brain.TaskBoard;
 import dev.xkmc.gensokyolegacy.content.entity.behavior.combat.YoukaiCombatManager;
+import dev.xkmc.gensokyolegacy.content.entity.behavior.task.combat.YoukaiSearchTargetTask;
 import dev.xkmc.gensokyolegacy.content.entity.youkai.BossYoukaiEntity;
+import dev.xkmc.gensokyolegacy.init.registrate.GLBrains;
 import dev.xkmc.l2serial.serialization.marker.SerialClass;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
@@ -12,6 +15,12 @@ public class MaidenEntity extends BossYoukaiEntity {
 	public MaidenEntity(EntityType<? extends MaidenEntity> pEntityType, Level pLevel) {
 		super(pEntityType, pLevel);
 		navCtrl.markHuman();
+	}
+
+	@Override
+	protected void constructTaskBoard(TaskBoard board) {
+		super.constructTaskBoard(board);
+		board.addBehaviorActivity(YoukaiSearchTargetTask.class, GLBrains.AT_HOME.get());
 	}
 
 	@Override
